bdf1f26cc749df570c61288534a976aab3f2b108
hayley-d <hayleydod@proton.me>
2025-06-06 15:21:42 +0200

    adding helper struct

--- miette/src/lex.rs
+++ miette/src/lex.rs
@@ -1,9 +1,68 @@
+pub struct Tokens {
+    tokens: std::collections::VecDeque<Token>,
+    contents: std::collections::VecDeque<char>,
+    current_line: usize,
+    current_token: Option<char>,
+}
+
+impl Tokens {
+    pub fn new(file_name: String) -> Tokens {
+        let contents: String = match std::fs::read_to_string(file_name) {
+            Ok(s) => s,
+            Err(e) => {
+                eprintln!("Failed to read the file, please check the file path");
+                return tokens.tokens;
+            }
+        };
+
+        Tokens{
+            tokens: std::collections::VecDeque::new(),
+            current_line: 1,
+            current_token: None,
+        }
+    }
+
+    pub fn advance(&mut self) -> bool {
+        self.current_token = self.tokens.pop_front();
+        
+        if self.current_token.is_some() {
+            if self.current_token.unwrap() == '\n' {
+                self.current_line = self.current_line + 1;
+                self.advance();
+            }
+            return true;
+        }
+
+        false
+    }
+}
+
 #[derive(Debug)]
 pub struct Token {
-    id: u128,
+    id: usize,
     kind: TokenKind,
     lexeme: String,
-    line: u128,
+    line: usize,
+}
+
+impl Token {
+    pub fn new(id: usize, kind: TokenKind, lexeme: String, line: usize) -> Self {
+        Token {
+            id,
+            kind,
+            lexeme,
+            line,
+        }
+    }
+
+    pub fn add_token(
+        tokens: &mut std::collections::VecDeque<Token>,
+        kind: TokenKind,
+        lexeme: String,
+        line: usize,
+    ) {
+        tokens.push_back(Token::new(tokens.len(), kind, lexeme, line))
+    }
 }
 
 impl std::fmt::Display for Token {
@@ -14,6 +73,55 @@ impl std::fmt::Display for Token {
             self.id, self.kind, self.lexeme, self.line
         )
     }
+}
+
+// Read in the file and parse for tokens
+pub fn scan_tokens(file_name: String) -> std::collections::VecDeque<Token> {
+    let tokens: Tokens = Tokens::new();
+
+
+
+    let mut contents: std::collections::VecDeque<char> = contents.chars().collect();
+
+    // Collects chars to be processes as a single token lexeme
+    let mut buffer: std::collections::VecDeque<char> = std::collections::VecDeque::new();
+    // Flag if currently iterating through a string "....."
+    let mut is_text: bool = false;
+
+    loop {
+        let current: char = match contents.pop_front() {
+            Some(c) => c,
+            None => break
+        }
+
+        match current {
+            '"' => {
+                if is_text {
+                    // end quote
+                    continue;
+                } else {
+                    // start quote
+                    is_text = true;
+                    continue;
+                }
+            },
+            '=' => {
+                let peek:char = match contents.front() {
+                    Some(c) => c,
+                    None => 
+                }
+
+                if peek == '=' {
+                    Token::add_token(&mut tokens, TokenKind::EqualEqual, current.to_string(), current_line);
+                } else {
+                    Token::add_token(&mut tokens,TokenKind::Equal, current.to_string(), current_line);
+                }
+            }
+        }
+
+    }
+
+    tokens
 }
 
 #[derive(Debug)]
